8c1d4f24192a870c1adfeec6b89ab16fa8cad279
ArfySaysHi <necroticowl@gmail.com>
2025-08-13 21:28:40 +0100

    react router added and basic route setup alongside flashbang protection for eyes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
 import "./App.scss";
+import { getCurrentWindow } from "@tauri-apps/api/window";
+import { useState, useEffect } from "react";
 import TitleBar from "./components/titlebar/TitleBar.tsx";
+import { RouterProvider } from "react-router";
+import router from "./routes.tsx";
 
 const App = () => {
+  const [isReady, setIsReady] = useState(false);
+  const appWindow = getCurrentWindow();
+
+  useEffect(() => {
+    const showWindow = async () => {
+      try {
+        await new Promise((res) => setTimeout(res, 100));
+
+        await appWindow.show();
+
+        setIsReady(true);
+      } catch (err) {
+        console.error("Error showing window:", err);
+        setIsReady(true);
+      }
+    };
+    showWindow();
+  }, []);
+
+  if (!isReady)
+    return (
+      <div className="anti-flash">
+        <div className="loading-screen">
+          <div className="spinner"></div>
+        </div>
+      </div>
+    );
+
   return (
     <main>
       <TitleBar />
+      <RouterProvider router={router} />
     </main>
   );
 };
---
+++ src/routes.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hey there</div>,
+  },
+]);
+
+export default router;
