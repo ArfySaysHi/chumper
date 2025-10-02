b2426f4f2475336c96831b275ce1aba5e829d459
ArfySaysHi <necroticowl@gmail.com>
2025-10-02 17:58:13 +0100

    okay back working on this a little after a break, implemented priorities creation

--- src-tauri/src/character/create.rs
+++ src-tauri/src/character/create.rs
@@ -1,4 +1,6 @@
-use crate::shared::Grade;
+use crate::priority_bundle::repository::{list_priority_bundles, PriorityBundleListParams};
+use crate::priority_bundle::PriorityBundle;
+use crate::shared::{group_by_key, Grade};
 use crate::{character::CharacterStatus, error::Result};
 use rusqlite::{named_params, Connection};
 use serde::{Deserialize, Serialize};
@@ -33,6 +35,45 @@ pub fn create(connection: &Connection, params: CharacterCreateParams) -> Result<
         ":metatype_id": &params.metatype_id,
     })?;
 
-    let _character_id = connection.last_insert_rowid();
+    let priority_bundles = list_priority_bundles(
+        connection,
+        &PriorityBundleListParams {
+            system: "Core".to_string(),
+        },
+    )?;
+    let bundle_map = group_by_key(priority_bundles, |pb| pb.id);
+
+    let character_id = connection.last_insert_rowid();
+    create_priorities(connection, params, &bundle_map, &character_id)?;
+
+    Ok(())
+}
+
+// Bookkeeping for later
+fn create_priorities(
+    connection: &Connection,
+    params: CharacterCreateParams,
+    bundle_map: &HashMap<i64, Vec<PriorityBundle>>,
+    character_id: &i64,
+) -> Result<()> {
+    let query = "INSERT INTO priorities (bundle_name, grade, priority_system, character_id)
+                 VALUES (:bundle_name, :grade, :priority_system, :character_id)";
+
+    let mut stmt = connection.prepare(&query)?;
+    for (bundle_id, option) in &params.priorities {
+        let pb_vec = bundle_map.get(&bundle_id);
+
+        if let Some(bundles) = pb_vec {
+            if let Some(current_bundle) = bundles.first() {
+                stmt.execute(named_params! {
+                    ":bundle_name": current_bundle.name,
+                    ":grade": option.grade,
+                    ":priority_system": &params.priority_system,
+                    ":character_id": character_id,
+                })?;
+            }
+        }
+    }
+
     Ok(())
 }
